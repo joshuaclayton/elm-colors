826bb714c3a98115382c04c31a561f9651f2b509
Joshua Clayton <joshua.clayton@gmail.com>
2016-03-23 06:18:52 -0400

    Improve search on homepage

--- web/static/js/elm/Colors/Home/View.elm
+++ web/static/js/elm/Colors/Home/View.elm
@@ -1,23 +1,41 @@
 module Colors.Home.View (view) where
 
 import Html exposing (..)
-import Html.Attributes exposing (type', value)
+import Html.Attributes exposing (type', value, pattern, placeholder)
 import Html.Events exposing (on, targetValue)
 import String exposing (trim)
 import Colors.Util.CustomEvent exposing (onSubmit)
 import Colors.Home.Model exposing (Model)
 import Colors.Home.Update exposing (Action, Action(..))
+import Regex exposing (regex, replace, HowMany(..))
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   form
     [ onSubmit address SubmitForm ]
-    [ input [ type' "text", on "input" targetValue (Signal.message address << setColorName) ] []
+    [ input
+        [ type' "text"
+        , placeholder "Hex code (e.g. FAA or FFAAAA)"
+        , pattern hexCodeValidationRegex
+        , on "input" targetValue (Signal.message address << setColorName)
+        ]
+        []
+    , em [] [ text " or " ]
+    , input [ type' "color", on "change" targetValue (Signal.message address << setColorName) ] []
     , input [ type' "submit", value "Show Color" ] []
     ]
 
 
 setColorName : String -> Action
 setColorName value =
-  SetSearchColor (trim value)
+  let
+    stripHash =
+      replace All (regex "#") (\_ -> "")
+  in
+    SetSearchColor <| (trim >> stripHash) value
+
+
+hexCodeValidationRegex : String
+hexCodeValidationRegex =
+  "[0-9a-fA-F]{3}|[0-9a-fA-F]{6}"
