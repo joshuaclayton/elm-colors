4ef26ace39f474ea76edf13e439ae9a37c80a51d
Joshua Clayton <joshua.clayton@gmail.com>
2016-03-24 04:45:47 -0400

    Update color picker when text input changes

--- web/static/js/elm/Colors/Home/View.elm
+++ web/static/js/elm/Colors/Home/View.elm
@@ -1,19 +1,20 @@
 module Colors.Home.View (view) where
 
 import Html exposing (..)
-import Html.Attributes exposing (type', value, pattern, placeholder)
+import Html.Attributes exposing (type', value, pattern, placeholder, class)
 import Html.Events exposing (on, targetValue)
 import String exposing (trim)
 import Colors.Util.CustomEvent exposing (onSubmit)
 import Colors.Home.Model exposing (Model)
 import Colors.Home.Update exposing (Action, Action(..))
 import Regex exposing (regex, replace, HowMany(..))
+import TinyColor
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   form
-    [ onSubmit address SubmitForm ]
+    [ class "color-search", onSubmit address SubmitForm ]
     [ input
         [ type' "text"
         , placeholder "Hex code (e.g. FAA or FFAAAA)"
@@ -21,10 +22,14 @@ view address model =
         , on "input" targetValue (Signal.message address << setColorName)
         ]
         []
-    , em [] [ text " or " ]
-    , input [ type' "color", on "change" targetValue (Signal.message address << setColorName) ] []
+    , input [ type' "color", value <| selectedValue model, on "change" targetValue (Signal.message address << setColorName) ] []
     , input [ type' "submit", value "Show Color" ] []
     ]
+
+
+selectedValue : Model -> String
+selectedValue model =
+  (Maybe.withDefault "" >> TinyColor.normalizeHexString) model.searchColor
 
 
 setColorName : String -> Action
